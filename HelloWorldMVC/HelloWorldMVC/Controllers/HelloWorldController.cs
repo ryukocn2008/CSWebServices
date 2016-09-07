3ed5b6f249f855d1c1055b6d5fc5e2837deece13
ryukocn2008 <ryukocn2008@yahoo.co.jp>
2016-09-07 08:20:23 -0700

    Added Parameters to Action Methods

---
+++ HelloWorldMVC/HelloWorldMVC/Controllers/HelloWorldController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HelloWorldMVC.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        //
+        // GET: /HelloWorld/
+        public string Index()
+        {
+            return "This is my default action...";
+        }
+
+        public string Welcome(string name, int numTimes = 1)
+        {
+            //return HtmlEncoder.Default.Encode($"Hello {name}, numTimes: {numTimes}");
+            return String.Format("Hello {0}, numTimes: {1}", name, numTimes);
+        }
+	}
+}
